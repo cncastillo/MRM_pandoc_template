f5e9545c637992804aa8ea4eb2eca678840593dc
Carlos Castillo Passi <cncastillo@uc.cl>
2024-09-11 12:42:01 -0300

    Filter: Only remove links pointing to markdown files

--- filters/remove-links.lua
+++ filters/remove-links.lua
@@ -1,5 +1,11 @@
 function Link(elem)
-    -- Convert the entire content of the link to a sequence of inlines
-    return elem.content
+    -- Check if the link points to a file with a .md extension
+    if string.sub(elem.target, -3) == ".md" then
+        -- Convert the entire content of the link to a sequence of inlines
+        return elem.content
+    else
+        -- Return the link as is
+        return elem
+    end
 end
 
